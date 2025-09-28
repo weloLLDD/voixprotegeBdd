843845472b5a0a81aa41ed4cd379bfdf44e7334f
john <johnpesene1992@gmail.com>
2025-09-28 17:49:14 +0100

    ajour backend 222

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+ import express from "express";
 import asyncHandler from "express-async-handler"; 
 import User from "../models/userModels.js";
 import generateToken from "../utils/generateToken.js";
